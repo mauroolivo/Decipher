42f252573ef660971b85ebe91581c31138a79ae6
Mauro Olivo <olivomauro@gmail.com>
2018-05-23 11:09:48 +0200

    Initial Commit

---
+++ Decipher/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Decipher
+//
+//  Created by Mauro Olivo on 23/05/18.
+//  Copyright © 2018 Mauro Olivo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Decipher/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Decipher
+//
+//  Created by Mauro Olivo on 23/05/18.
+//  Copyright © 2018 Mauro Olivo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
